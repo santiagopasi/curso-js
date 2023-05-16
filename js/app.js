77a0fc62ef066efbfdbaec1aa3dfcaa937ef5bdf
Santiago <santiagopasi111@gmail.com>
2023-05-16 17:34:49 -0300

    Actividad 1.1

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 control1 = true
 
 while (control1) {
-    let inicio = prompt("Oprima 1 para utilizar el concatenador /n Oprima 2 para utilizar el multiplicador de palabras /n Oprima 3 para calcular el área de una circunferencia. ")
+    let inicio = prompt("Oprima 1 para utilizar el concatenador")
 
     if (inicio == 1) {
+        
+        function concatenador(frase1,frase2,times) {
+            
+            let solucion2 = (frase1 + " " + frase2 + " ").repeat(times) 
+            alert(solucion2)
+        
+        }
         let frase1 = prompt("Ingrese la primera frase a concatenar")
         let frase2 = prompt("Ingrese la segunda frase a concatenar")
         let times = parseFloat(prompt("Ingrese la cantidad de veces que quiere que se repita la concatenación."))
-        function concatenador(frase1,frase2,times) {
-            
-            
-            for (i = 0; i < times; i++) {
-                solucion2 = ""
-                let solucion1= frase1 + "  " + frase2
-                let solucion2 = soluciion2 + solucion1
-                
-        }
-        console.log(solucion2);
-            
-        }
         concatenador(frase1,frase2,times)
-        control1=false
+
+        
+    }else{
+        let inicio2 = prompt("Escriba ESC si quiere salir del programa o cualquier otra tecla para volver a empezar.")
+        if (inicio2 == "ESC") {
+            control1 = false
+            
+        }
+
     }
    
 }
---
+++ js/test.js
@@ -0,0 +1,8 @@
+for (i = 0; i < 3; i++) {
+    solucion2 = ""
+    let solucion1= "asd" + "  " + "tete"
+    let solucion2 = solucion2 + solucion1
+        }
+
+
+console.log(solucion2);
