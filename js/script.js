6cd0b7b165e0866532bce0e246feecbb887360a4
Santiago <santiagopasi111@gmail.com>
2023-06-11 20:02:29 -0300

    Entrega numero 2.2

--- js/script.js
+++ js/script.js
@@ -1,71 +1,77 @@
+class Usuario {
+    constructor(id,nombre){
+        this.id = id
+        this.nombre = nombre
+        this.aporte = []
+    }
 
-/* let subtitulo = document.getElementById('subtitulo')
-subtitulo.innerHTML = 'AQUI se actualizará tu nombre después del tiempo indicado'
-
-let nombre = prompt('Indique su nombre')
-let tiempo = parseFloat(prompt('Indique en cuántos segundos quiere que se actualice el subtítulo con su nombre '))
-
-let data = new  Date()
-
-
-function cambio(nombre,tiempo) {
-    subtitulo.innerHTML = nombre + ' solicitó actualizar el subtítulo y se ejecutó en ' + tiempo + ' segundos.'
-    for (let index = 0; index < 1000000; index++) {
-        console.log('haciendo tiempo');
-        let newData = new Date()
-        let dif = (newData-data)/1000
-        if (dif==tiempo) {
-            break
-            
-        }
-        
+    agregarAporte (aporte){
+        this.aporte.push(aporte)
     }
 }
-cambio ( nombre,tiempo) */
 
-
-
-function myFunction() {
-    let text = "Quieres seguir agregando productos?\nApretá OK o Cancel.";
-    if (confirm(text) == true) {
-        text = 'you pressed ok'
-        
-        let control = true
-        return control
-        
-    } else {
-        text = 'Cancel'
-        
-        let control = false
-        return false
-        
-    }
-
-  }
-
-let control = true
 let index = 0
-let objetos = {}
-while (control==true) {
+let userList = []
+opcion = 0
+while (opcion != 3) {
+    let nombreUser = prompt('Indique su nombre')
+    let newUser = new Usuario(index,nombreUser)
+    userList.push(newUser)
+    index += 1
+    opcion = parseInt(prompt(
+        newUser.nombre + ' usted quiere:\n' +
+        '1. Ingresar un nuevo aporte con este user.\n' +
+        '2. Ingresar aportes con nuevo user.\n' +
+        '3. Salir e imprimir listado de usuarios con sus aportes.'
+    ));
+    if (opcion==1) {
+        monto = parseFloat(prompt('Agregar nuevo aporte con este usuario'));
+            if (isNaN(monto)) {
+                alert('Ingrese un valor numérico! Ahora volverá a empezar el menu.'); 
+               
+            } else {
+                newUser.agregarAporte(monto);   
+                opcionMonto= confirm('Desea agregar más aportes con este usuario?')
+                if (opcionMonto == true) {
+                    monto = parseFloat(prompt('Agregar nuevo aporte con el mismo usuario'));
+                    if (isNaN(monto)) {
+                        alert('Ingrese valor valido'); 
+               
+                    } else {
+                        newUser.agregarAporte(monto); 
+                    
+                }
+            } else {
+                alert('Volverás al menu principal y te preguntará por un nuevo user!')
+            }
+            }
+    if (opcion==2) {
+        let nombreUser = prompt('Indique su nombre')
+        let newUser = new Usuario(index,nombreUser)
+        monto = parseFloat(prompt('Agregar nuevo aporte con este nuevo usuario'));
+            if (isNaN(monto)) {
+                alert('Ingrese valor valido'); // si no es valido el input vuelve al menu raiz
+               
+            } else {
+                newUser.agregarAporte(monto);   
+            };
+    }
+    if (opcion == 3) {
+        break
         
-        let nombre = prompt('Indique el nombre del producto a añadir a la lista')
-        let precio = prompt('Indique el precio de ese producto')
-        objetos += {id:index,nombre_objecto:nombre,precio_objecto:precio }
-        index += 1
-        
-        let texto = myFunction()
-        
-        if (texto == false) {
-            control = false
-        }
-     
-    
-}
-console.log(JSON.stringify(objetos));
-/* for (const objeto in objetos) {
-    console.log(objeto.id);
-} */
+    }
+    }
 
-/* for (const objeto of objetos) {
-    console.log(objeto);
-} */
+    }      
+    
+let subtitulo = document.getElementById('subtitulo')
+
+
+
+for (const user of userList) {
+        subtitulo.innerHTML += 'User con id '+ user.id + ' nombre '+  user.nombre + ' realizó un aporte de ' + user.aporte + '\n'
+        console.log('User con id '+ user.id + ' nombre '+  user.nombre + ' realizó un aporte de ' + user.aporte);
+}
+
+
+
